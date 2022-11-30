f2ac430c2b82f3ba34a8484c6bb69a516174b77a
xakojwm <wyattmorris24@gmail.com>
2022-11-30 05:39:08 -0800

    separated code and updated make

--- Implant/tasks.cpp
+++ Implant/tasks.cpp
@@ -5,30 +5,35 @@
 #include <iostream>
 #include <windows.h>
 
+
 /* start of implant functions*/
 
-void FileUpload(char * Path);
+void FileUpload(char* Path){}
 
-void FileDownload();
+void FileDownload(){}
 
-void SitAware();
+void SitAware(){}
 
-void Stealer();
+void Stealer(){}
 
 /*Task Dispatch*/
-using pfunc = void (*)(string);
-unordered_map<char *, pfunc> dispatch_table;
-dispatch_table["SitAware"] = &SitAware;
-dispatch_table["FileDown"] = &FileDownload;
-dispatch_table["FileUp"] = &FileUpload;
-dispatch_table["Stealer"] = &Stealer;
+using pfunc = void (*)(std::string);
+std::unordered_map<std::string, pfunc> dispatch_table;
+
+void DispatchTableInit(){
+    dispatch_table["SitAware"] = (pfunc)SitAware;
+    dispatch_table["FileDown"] = (pfunc) FileDownload;
+    dispatch_table["FileUp"] = (pfunc)FileUpload;
+    dispatch_table["Stealer"] = (pfunc)Stealer;
+    }
+
 
 //these functions are listd above but maybe we should break it up
 //tasks are lists of tasks structured [cmd: , args:, taskid: ]
-void tasks_dispatch(tasks);
+void tasks_dispatch(char ** tasks);
 //create results arr and return
 
-void task_do(task);
+void task_do(char *task);
 //get cmd, args, id
 //choose execute
 // read output from file and return res
@@ -40,3 +45,17 @@ void tasking();
 /* other helper functions necessary*/
 
 void readFile(char * Path, char * buf);
+
+
+int main(int argc, char* argv[]){
+
+    DispatchTableInit();
+    for (auto x : dispatch_table) {
+	std::cout << x.first << " " <<
+			x.second << "\n";
+}
+
+
+
+    return 0;
+}
