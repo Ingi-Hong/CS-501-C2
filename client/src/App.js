cd04db849baf2e9917f9675f42a7cf47ac99afd0
Catherine <quachcat@bu.edu>
2022-12-01 19:20:41 -0500

    Revert "Merge branch 'main' of https://github.com/Ingi-Hong/CS-501-C2"
    This reverts commit f2e3e1074cd3a8ff02971f065d25675fa98b6945, reversing
    changes made to 206c7c3d68a399246e4f3b4b4dccfc2a9b2c5b8c.

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,29 @@
 import React from "react";
-import { redirect } from "react-router-dom";
 import "./App.css";
 import LoginForm from "./Components/LoginForm";
-import { Router, Route, Routes, Outlet, BrowserRouter } from "react-router-dom";
-import { bake_cookie, read_cookie, delete_cookie } from "sfcookies";
+import { Route, BrowserRouter } from "react-router-dom";
+import Cookies from 'js-cookie';
 import Home from "./Components/Home";
 import NavBar from "./Components/NavBar";
-import AuthService from "./Components/AuthService";
 
-const DefaultPage = () => {
-
-  const isLoggedIn = AuthService.isLoggedIn();
-  console.log(isLoggedIn)
-  if (!isLoggedIn){
-    return redirect("/login");
-  }
-  return (
-    <div>
-      <NavBar />
-      <Outlet />
-    </div>
-  );
-};
+const DefaultPage = () => (
+  <div>
+    <NavBar />
+    <BrowserRouter>
+      <Route path="/main" element={<Home />}/>
+    </BrowserRouter>
+  </div>
+);
 
 function App() {
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultPage />}>
-          <Route path="/" element={<Home />} />
-        </Route>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Route element={<DefaultPage />} />
         <Route path="/login" element={<LoginForm />} />
-      </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </React.StrictMode>
   );
 }
 
