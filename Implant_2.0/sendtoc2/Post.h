a3ed712030d7cc0ce481ece03d3ec59bdbb6a6cf
FeiyuWang <36805094+FeiyuWang@users.noreply.github.com>
2022-12-18 23:54:42 -0500

    Update Post.h

--- Implant_2.0/sendtoc2/Post.h
+++ Implant_2.0/sendtoc2/Post.h
@@ -10,7 +10,10 @@
 
 using namespace std;
 
-/* Functions */
-void sendToC2(string, string);
+/*
+* example use case: sendToC2(path, url, filename, taskid) where path is the path to the file(include the filename), url is the url 
+* with format ip:port/endpoint, filename being the file name C2 going to recive after calling get file name in python code
+*/
+void sendToC2(string, string, string, string);
 
 #endif
