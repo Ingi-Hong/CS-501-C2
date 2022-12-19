cd75e42a937c00ff6b613657b9d972823d35b54c
FeiyuWang <36805094+FeiyuWang@users.noreply.github.com>
2022-12-19 13:37:09 -0500

    Update xor.cpp

--- Implant_2.0/xor/xor.cpp
+++ Implant_2.0/xor/xor.cpp
@@ -1,8 +1,9 @@
 #include "xor.h"
 using namespace std;
 
+int KEY = 0x42;
 //XORs a string with a key
-std::string xor_string(std::string& s, int key) {
+std::string xor_string(std::string s, int key = KEY) {
     std::string result = s;
     for (int i = 0; i < s.length(); i++) {
         result[i] = s[i] ^ key;
@@ -11,7 +12,7 @@ std::string xor_string(std::string& s, int key) {
 }
 
 //XORs a wstring with a key
-std::wstring xor_wstring(wchar_t* s, int key) {
+std::wstring xor_wstring(wchar_t* s, int key = KEY) {
     std::wstring result = s;
     for (int i = 0; i < wcslen(s); i++) {
         result[i] = s[i] ^ key;
